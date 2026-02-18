c1abe69a33717c46485e6569f77db0ae4300fb13
deepakfalke <deepfalke@gmail.com>
2026-02-18 18:14:32 +0530

    post api

--- tests/ApiGet.spec.ts
+++ tests/ApiGet.spec.ts
@@ -1,14 +1,23 @@
 import {test,expect} from '@playwright/test';
-test('test', async ({ request }) => {
+test('getApi', async ({ request }) => {
    // await page.goto('https://jsonplaceholder.typicode.com/todos/1');
 
     const response = await request.get('https://jsonplaceholder.typicode.com/todos/1');
     const body  = await response.json();
     console.log(body);
     const status = response.status();
+    const responsetext = await response.text();
+    console.log(responsetext);
     console.log(status);
-    expect(response.status()).toBe(200);
-   // const headers = response.headers();
-    //console.log(headers);
 
+    // validation 
+    expect(response.status()).toBe(200);
+    expect(response.status()).not.toBe(400);
+     expect (responsetext).toContain("delectus aut autem");   
+ 
+    expect (body.id).toBe(1);
+    expect(body.title).toBe("delectus aut autem");
+
+
+    
 })
---
+++ tests/ApiPost.spec.ts
@@ -0,0 +1,70 @@
+import {test,expect} from '@playwright/test';
+test('postApi', async ({ request }) => {
+   
+const postdata={
+    "username" : "admin",
+    "password" : "password123"
+       }
+
+
+    const response = await request.post('https://restful-booker.herokuapp.com/auth',
+        {headers:{'Content-Type': 'application/json'},data:postdata })
+  
+        const status = response.status();
+        const responsetext = await response.text();
+        console.log(responsetext);
+        console.log(status);
+        console.log(await response.json());
+
+        // validation
+
+        const responsebody = await response.json();
+        expect(response.status()).toBe(200);
+        expect(responsebody.token).not.toBeNull();
+
+
+
+
+   
+   
+
+    
+
+})
+test('postBookingApi', async ({ request }) => {
+
+const bookingData={
+    "firstname" : "Jim",
+    "lastname" : "Brown",
+    "totalprice" : 111,
+    "depositpaid" : true,
+    "bookingdates" : {
+        "checkin" : "2018-01-01",
+        "checkout" : "2019-01-01"
+    },
+    "additionalneeds" : "Breakfast"
+}
+
+
+    const response = await request.post('https://restful-booker.herokuapp.com/booking',
+        {headers:{'Content-Type': 'application/json'},data:bookingData })
+
+        const status = response.status();
+        const responsedata = await response.json();
+        console.log(responsedata);
+        console.log(status);
+        console.log(await response.json()); 
+
+        // validation
+        expect(response.status()).toBe(200);
+        expect(responsedata.bookingid).not.toBeNull();
+        expect(responsedata.booking.firstname).toBe("Jim");
+        expect(responsedata.booking.lastname).toBe("Brown");
+        expect(responsedata.booking.totalprice).toBe(111);
+        expect(responsedata.booking.depositpaid).toBe(true);
+        expect(responsedata.booking.bookingdates.checkin).toBe("2018-01-01");
+        expect(responsedata.booking.bookingdates.checkout).toBe("2019-01-01");
+        expect(responsedata.booking.additionalneeds).toBe("Breakfast"); 
+
+}
+)
